8ee587bbc388571734fdecb0a658be7b847cc73d
RenaTin1 <31945448+RenaTin1@users.noreply.github.com>
2017-09-28 00:35:31 -0400

    QL Lab #4

---
+++ LabNumber4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNumber4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lab #3- Serena Tindle 
+            bool ContinueOn = true;  //boolean expression
+            while (ContinueOn == true)
+            {
+                int Num1;  //declare integers
+                long fact;
+               
+                Console.WriteLine("Please enter a number between 1 and 10:");
+                Num1 = int.Parse(Console.ReadLine()); //user input will be factored 
+                fact = Num1;
+
+                for (int i = 1; i <= Num1; i++)
+
+                {
+                    fact = (fact * i); //put in brackets, outside for loop
+                }
+                Console.WriteLine("Factorial of the given number is:" + fact); //output
+            }
+        }
+    }
+}
